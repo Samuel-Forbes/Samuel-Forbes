24feb76a0456f974ac06a7d3dd53fc020b22c113
Samuel Forbes <sam.forbes@live.com>
2018-02-24 16:11:22 +1300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var one = 'Samuel Thomas Forbes'; 
+var two = '+64 (0)27 257 0995'; 
+var speed = '1000'; 
+
+var first = true; function setTitle() { if(first) document.title = one; else document.title = two; first = !first; } setInterval(setTitle, speed); 
