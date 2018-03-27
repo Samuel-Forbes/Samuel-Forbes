8fb0693792436e9c7decfb9f84a68783e3e693ac
Samuel Forbes <sam.forbes@live.com>
2018-03-27 21:59:41 +1300

    2.8

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ var one = 'Samuel Forbes';
 var two = '+64 (0)27 257 0995'; 
 var speed = '1000'; 
 
-var first = true; function setTitle() { if(first) document.title = one; else document.title = two; first = !first; } setInterval(setTitle, speed); 
+var first = true; function setTitle() { if(first) document.title = one; else document.title = two; first = !first; }; setInterval(setTitle, speed); 
